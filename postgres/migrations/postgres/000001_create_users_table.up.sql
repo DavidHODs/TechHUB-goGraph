ec6f511460a39c86296e1e00575282cea454c871
DavidHODs <davidoluwatobi41@gmail.com>
2022-04-15 12:22:08 +0000

    proper schema definition

--- postgres/migrations/postgres/000001_create_users_table.up.sql
+++ postgres/migrations/postgres/000001_create_users_table.up.sql
@@ -1,8 +1,52 @@
-CREATE TABLE IF NOT EXISTS users (
-	id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
+BEGIN;
+
+CREATE EXTENSION IF NOT EXISTS citext;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+
+CREATE SCHEMA IF NOT EXISTS tech;
+
+-- CREATE OR REPLACE FUNCTION set_updated_at() RETURNS trigger AS $$
+-- BEGIN
+--   IF (
+--     NEW IS DISTINCT FROM OLD AND
+--     NEW.updated_at IS NOT DISTINCT FROM OLD.updated_at
+--   ) THEN
+--     NEW.updated_at := current_timestamp;
+--   END IF;
+--   RETURN NEW;
+-- END;
+-- END;
+
+
+CREATE TABLE IF NOT EXISTS tech.users(
+	id uuid NOT NULL DEFAULT gen_random_uuid () PRIMARY KEY,
 	name VARCHAR (127) NOT NULL,
 	email VARCHAR (127) NOT NULL,
 	password VARCHAR (127) NOT NULL,
 	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
-	updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
+	updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp
 );
+
+-- CREATE TABLE IF NOT EXISTS tech.tags(
+-- 	id uuid NOT NULL DEFAULT gen_random_uuid () PRIMARY KEY UNIQUE,
+-- 	name VARCHAR (127) NOT NULL
+-- );
+
+-- CREATE TABLE IF NOT EXISTS tech.posts (
+-- 	id uuid NOT NULL DEFAULT gen_random_uuid () PRIMARY KEY,
+-- 	body VARCHAR (1027) NOT NULL,
+-- 	shared_body VARCHAR (1027),
+-- 	image VARCHAR (127),
+-- 	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
+-- 	update_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
+-- 	shared_at TIMESTAMP WITH TIME ZONE,
+-- 	author uuid NOT NULL REFERENCES tech.users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+-- 	shared_user uuid REFERENCES tech.users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+-- 	likes uuid REFERENCES tech.users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+-- 	Dislikes uuid REFERENCES tech.users (id) ON DELETE CASCADE ON UPDATE CASCADE,
+-- 	Tags uuid REFERENCES tech.tags (id) ON DELETE CASCADE ON UPDATE CASCADE
+-- );
+
+END;
