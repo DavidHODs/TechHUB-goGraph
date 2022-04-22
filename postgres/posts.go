ec0e07a016f5163274d79a4271dc744b8334779d
DavidHODs <davidoluwatobi41@gmail.com>
2022-04-22 15:13:56 +0000

    save post functionality

---
+++ postgres/posts.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"errors"
+
+	"github.com/DavidHODs/TechHUB-goGraph/utils"
+)
+
+
+func SavePost(body, sharedBody, image string) (string, string, error) {
+	if body == "" {
+		utils.HandleError(errors.New("post can not be blank"), false)
+		return "", "", errors.New("post can not be blank")
+	}
+
+	postLength := len(body)
+	if postLength > 1024 {
+		utils.HandleError(errors.New("Post can not be longer than 1024 words"), false)
+		return "", "", errors.New("Post can not be longer than 1024 words")
+	}
+
+	stmt, err := Db.Prepare(`INSERT INTO tech.posts(body, shared_body, image)
+							VALUES($1, $2, $3)
+							RETURNING id`)
+	if err != nil {
+		utils.HandleError(err, false)
+		return "", "", errors.New("Something went wrong, try reposting")
+	}
+
+	defer stmt.Close()
+
+	var id string = ""
+
+	err = stmt.QueryRow(body, sharedBody, image).Scan(&id)
+	if err != nil {
+		utils.HandleError(err, false)
+		return "", "", errors.New("Something went wrong, try reposting")
+	}
+
+	return id, body, nil
+}
